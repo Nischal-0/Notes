5b6bfdf9a2e84b5eccaa7be0ac02368aa6a94f10
Nischal Dhamala <nischaldhamala0@gmail.com>
2023-07-30 14:09:40 +0545

    Notes update

--- Notes/models.py
+++ Notes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_important = models.BooleanField(default=False)
+    
--- Notes/views.py
+++ Notes/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Note
 
 # Create your views here.
+
+def index(request):
+    tasks= Note.objects.all()
+    
+    total_task=tasks.count()
+    
+    context={
+        'tasks':tasks,
+        'total_task':total_task
+    }
+    
+    return render(request,'index.html',context)
+    
+def create(request):
+    if request.method=="POST":
+        title=request.POST.get('title')
+        description=request.POST.get('description')
+
+        if title and description:
+            Note.objects.create(title=title,description=description)
+            return redirect('index')
+        else:
+            context={
+                'error': 'Both fields required'
+            }
+            return render(request,'create.html',context)
+
+    
+    return render(request,'create.html')
+
+
+def edit(request,id):
+    
+    task=Note.objects.get(id=id)
+
+    if request.method == "POST":
+        title=request.POST.get('title')
+        description=request.POST.get('description')
+        
+        task.title = title
+        task.description = description
+        task.save()
+        
+        return redirect('index')
+        
+    context={
+        'task':task
+    }
+    
+    return render (request,'edit.html',context)
+
+
+def delete(request,id):
+    if request.method == "POST":
+        task=Note.objects.get(id=id)
+        task.delete()
+        return redirect ('index')
+    
+    
+ 
